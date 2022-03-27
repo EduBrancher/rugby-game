88fda9b760dd4bd20d6556f85ab80649ebef24a1
edu <EduBrancher>
2022-03-27 11:33:33 -0300

    src/attacker.c: Implement attacker strategy
    This commit implements a simple semi-random attacker strategy
    that uses spy once to avoid the defender.

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,18 +13,117 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+
+// limit is a double between 0 and 1 that determines the maximum value of the random
+// numbers generated. For instance, if it is 0.5, this generates a number between 0 
+// and 0.5.
+
+// OBS: Essa funcao nao deveria estar repetida aqui, deveria estar em um modulo util.h
+// que agruparia funcoes genericamente uteis para varios propositos
+// OU um modulo player.h que seria um objeto generico do qual attacker e defender herdam
+double random_number(double limit){
+    limit = abs(limit);
+    srand(time(0));
+    return (double) rand() / ((double) RAND_MAX * (double)(1 / limit));
+}
+
+//returns integer between 0 and limit - 1.
+int random_integer(int limit){
+    limit = abs(limit);
+    return (int) randomNumber(1) * limit;
+    
+}
+
+bool equal_directions(direction_t a, direction_t b){
+    return (a.i == b.i && a.j == b.j);
+}
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
+  static int rounds = 0;
+  static bool spy_used = false;
+  static bool spy = false;
+  static double spy_chance = 0.2;
+  static int wandering_steps = 0;
+  static double wandering_chance = 0.9;
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t previous_direction = DIR_STAY;
+  static direction_t wandering_dir = DIR_UP;
+  static direction_t dirs[] = {DIR_UP, DIR_UP_RIGHT, DIR_RIGHT, DIR_DOWN_RIGHT,
+                               DIR_DOWN, DIR_DOWN_LEFT, DIR_LEFT, DIR_UP_LEFT};
+  static int escape_index = 0;
+  
+  if (rounds > 3){
+      wandering_chance = 0.2;
+  }
+  rounds++;
+
+  //check if we are stuck, and unstuck
+  if (equal_positions(attacker_position, previous_position)){
+      if (equal_directions(dirs[escape_index], previous_direction)){
+          escape_index++;
+          previous_direction = dirs[escape_index];
+          return dirs[escape_index];
+      }
+  }
+
+  if (spy_used == false && random_number(1) < spy_chance && rounds > 3){
+      spy = true;
+      wandering_chance = 0;
+  }
+  
+  if (spy){
+      position_t defender_position = get_spy_position(defender_spy);
+      int vertical_distance = attacker_position.i - defender_position.i;
+      int horizontal_distance = attacker_position.j - defender_position.j;
+      if (vertical_distance > 0){ //defender is above
+          if (horizontal_distance >= -2 && vertical_distance <= 2 ) {
+              wandering_dir = (direction_t) DIR_DOWN_LEFT; //defender is very close so we go down and away
+          }
+          else if (horizontal_distance > -3 && vertical_distance <= 3){
+              wandering_dir = (direction_t) DIR_DOWN; //defender is close so we go down
+          }
+          else{
+              wandering_dir = (direction_t) DIR_UP_RIGHT;
+          }
+      }
+      else if (vertical_distance <= 0){ //below or on our line
+          if (horizontal_distance >= -2 && vertical_distance >= -2) {
+              wandering_dir = (direction_t) DIR_UP_LEFT; //defender is very close so we go up and away
+          }
+          else if (horizontal_distance >= -3 && vertical_distance >= -3) {
+              wandering_dir = (direction_t) DIR_UP; //defender is close so we go up
+          }
+          else {
+              wandering_dir = (direction_t) DIR_UP_RIGHT;
+          }
+      }
+      wandering_steps = 3;
+  }
+  
+  // check if we wander
+  double wander = random_number(1);
+
+  if (wander < wandering_chance && wandering_steps == 0){
+      printf("Attacker wandering\n");
+      wandering_steps = 3;
+      wandering_dir = (direction_t) dirs[random_integer(8)];
+  }
+
+  //process wander
+  if (wandering_steps > 0){
+      wandering_steps--;
+      previous_direction = wandering_dir;
+      return wandering_dir;
+  }
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  previous_direction = (direction_t) DIR_RIGHT;
+  return (direction_t) DIR_RIGHT; 
 }
 
 /*----------------------------------------------------------------------------*/
